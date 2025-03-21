7679b48c0f4d3a0f1631545ff8ed831961a2ad84
kamend0 <mendoza.kollin@gmail.com>
2025-03-21 12:09:40 -0500

    archive first batch of code

--- constants.py
+++
@@ -1,5 +0,0 @@
-# Order does matter here
-FACES = ["jack", "queen", "king"]
-UNIQUE_CARDINALITIES = {"ace": 1, "jack": 11, "queen": 12, "king": 13}
-UNIQUE_VALUES = {"ace": 11, "jack": 10, "queen": 10, "king": 10}
-SUITS = ["spade", "diamond", "club", "heart"]
